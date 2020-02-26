59394b65e8fc80b830c78fff21075064b7fc3002
MaiDinhChien <vocabulary.723@gmail.com>
2020-02-26 15:41:18 +0700

    init project

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/chuacacbienhangsodungchung/WebConstant.java
@@ -0,0 +1,4 @@
+package vn.myclass.web.logic.common.chuacacbienhangsodungchung;
+//Chua tat ca cac hang so dung chung cho tang web logic nay
+public class WebConstant {
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+<title>test project</title>
+</head>
+
+<body>
+
+<h1>test</h1>
+</body>
+</html>
